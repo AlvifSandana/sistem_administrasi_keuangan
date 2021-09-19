dc085fb2ac800d9bce2c50d5605632d579b27495
AlvifSandana <asm240800@gmail.com>
2021-09-19 15:10:34 +0700

    table user migration

--- app/Database/Migrations/2021-09-19-044225_CreateTableUser.php
+++ app/Database/Migrations/2021-09-19-044225_CreateTableUser.php
@@ -8,11 +8,43 @@ class CreateTableUser extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_user' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'nama' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+            ],
+            'username' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+                'unique'            => true,
+            ],
+            'email' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+                'unique'            => true,
+            ],
+            'password' => [
+                'type'              => 'VARCHAR',
+            ],
+            'created_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('user');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('user');
     }
 }
