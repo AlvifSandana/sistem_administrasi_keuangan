568a682e89ad42062008d555ce6631654c9a4a75
AlvifSandana <asm240800@gmail.com>
2021-09-19 15:11:07 +0700

    table jurusan migration

--- app/Database/Migrations/2021-09-19-044324_CreateTableJurusan.php
+++ app/Database/Migrations/2021-09-19-044324_CreateTableJurusan.php
@@ -8,11 +8,30 @@ class CreateTableJurusan extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_jurusan' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'nama_jurusan' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+            ],
+            'created_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id_jurusan', true);
+        $this->forge->createTable('jurusan');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('jurusan');
     }
 }
