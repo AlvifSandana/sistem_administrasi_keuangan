d45d881b63d6db75bb9373a0ca3cff2c5153239d
AlvifSandana <asm240800@gmail.com>
2021-09-19 14:50:26 +0700

    add admin template

---
+++ app/Views/layout/master.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistem Administrasi Keuangan</title>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+  <!-- Wrapper -->
+  <div class="wrapper">
+    <?= $this->include('layout/navbar') ?>
+    <?= $this->include('layout/sidebar') ?>
+    <div class="content-wrapper">
+      <?= $this->renderSection('content-header') ?>
+      <?= $this->renderSection('content-body') ?>
+    </div>
+    <?= $this->include('layout/footer') ?>
+  </div>
+  <!-- ./Wrapper -->
+  <!-- Script -->
+  <?= $this->include('layout/script') ?>
+  <?= $this->renderSection('custom-script') ?>
+  <!-- ./Script -->
+</body>
+</html>
