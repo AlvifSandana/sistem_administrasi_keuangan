608a87cd2e94222731504ea43efa6fb9772989dc
AlvifSandana <asm240800@gmail.com>
2021-09-19 15:10:52 +0700

    table progdi migration

--- app/Database/Migrations/2021-09-19-044319_CreateTableProgdi.php
+++ app/Database/Migrations/2021-09-19-044319_CreateTableProgdi.php
@@ -8,11 +8,30 @@ class CreateTableProgdi extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_progdi' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'nama_progdi' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+            ],
+            'created_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id_progdi', true);
+        $this->forge->createTable('progdi');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('progdi');
     }
 }
