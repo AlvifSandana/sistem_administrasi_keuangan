c46997f002aa9a58c6e0ec56bf0f2dda1f7b6fde
AlvifSandana <asm240800@gmail.com>
2021-09-19 15:11:59 +0700

    table semester migration

--- app/Database/Migrations/2021-09-19-044350_CreateTableSemester.php
+++ app/Database/Migrations/2021-09-19-044350_CreateTableSemester.php
@@ -8,11 +8,30 @@ class CreateTableSemester extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_semester' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'nama_semester' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+            ],
+            'created_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id_semester', true);
+        $this->forge->createTable('semester');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('semester');
     }
 }
