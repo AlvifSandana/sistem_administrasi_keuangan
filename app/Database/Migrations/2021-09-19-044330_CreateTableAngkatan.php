8113be5c9c2f942d3edd785b880a0507b9af2172
AlvifSandana <asm240800@gmail.com>
2021-09-19 15:11:43 +0700

    table angkatan migration

--- app/Database/Migrations/2021-09-19-044330_CreateTableAngkatan.php
+++ app/Database/Migrations/2021-09-19-044330_CreateTableAngkatan.php
@@ -8,11 +8,30 @@ class CreateTableAngkatan extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_angkatan' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'nama_angkatan' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+            ],
+            'created_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type'              => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id_angkatan', true);
+        $this->forge->createTable('angkatan');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('angkatan');
     }
 }
