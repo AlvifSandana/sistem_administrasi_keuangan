8cf556b651685dd827a49239dfabd3b29be346a3
AlvifSandana <asm240800@gmail.com>
2021-09-23 13:30:55 +0700

    TODO: pambayaran view

---
+++ app/Views/pages/pembayaran/index.php
@@ -0,0 +1,27 @@
+<?= $this->extend('layout/master') ?>
+
+<?= $this->section('content-header') ?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Pembayaran</h1>
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Pembayaran</li>
+        </ol>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<?= $this->endSection() ?>
+
+<?= $this->section('content-body') ?>
+<?= $this->endSection() ?>
+
+<?= $this->section('custom-script') ?>
+<?= $this->endSection() ?>
