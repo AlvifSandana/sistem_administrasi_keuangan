dbb2e1c6f0f148868a79838872fe8774f8ca5010
AlvifSandana <asm240800@gmail.com>
2021-09-23 13:34:15 +0700

    add detail tagihan modal, show item paket, total tagihan

--- app/Views/pages/tagihan/index.php
+++ app/Views/pages/tagihan/index.php
@@ -46,15 +46,14 @@
             <div class="row">
               <div class="col">
                 <h5 class="h5">Hasil Pencarian</h5>
-                <table class="table">
+                <table class="table table-hover">
                   <thead class="text-center">
                     <th>ID</th>
                     <th>NIM</th>
                     <th>NAMA MAHASISWA</th>
-                    <th>SEMESTER</th>
                     <th>ACTION</th>
                   </thead>
-                  <tbody class="text-center">
+                  <tbody class="text-center" id="list_tagihan">
                   </tbody>
                 </table>
               </div>
@@ -65,35 +64,101 @@
     </div>
   </div>
 </section>
+<div class="modal fade bd-example-modal-lg" id="modalDetailTagihan" tabindex="-1" role="dialog" aria-labelledby="modalDetailTagihan" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Detail Tagihan</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <label for="detail_nim">NIM</label>
+          <input type="text" class="form-control" name="nim" id="detail_nim" disabled/>
+        </div>
+        <div class="form-group">
+          <label for="detail_nim">NAMA MAHASISWA</label>
+          <input type="text" class="form-control" name="nama_mhs" id="detail_nama_mhs" disabled/>
+        </div>
+        <div class="form-group">
+          <label for="detail_nim">PAKET</label>
+          <input type="text" class="form-control" name="paket" id="detail_nama_paket" disabled/>
+        </div>
+        <label>ITEM PAKET</label>
+        <table class="table table-hover table-bordered">
+          <thead class="text-center">
+            <th>ID</th>
+            <th>Item Pembayaran</th>
+            <th>Nominal</th>
+            <th>Keterangan</th>
+          </thead>
+          <tbody class="text-center" id="list_item"></tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 <?= $this->endSection() ?>
 
 <?= $this->section('custom-script') ?>
 <script>
-  function searchMhs(){
+  function searchMhs() {
     var nim = $("#nim").val();
     $.ajax({
-      url: "<?php site_url() ?>" + "/tagihan/search/"+ nim,
+      url: "<?php site_url() ?>" + "/tagihan/search/" + nim,
       type: "GET",
-      dataType: "JSON", 
-      success: function(data){
+      dataType: "JSON",
+      success: function(data) {
         if (data == null || data.id_tagihan == undefined) {
           alert("Data tidak ditemukan");
         } else {
+          // hasil pencarian, baris baru
           var row = `
           <tr class="">
             <td>${data.id_tagihan}</td>
             <td>${data.nim}</td>
             <td>${data.nama_mahasiswa}</td>
+            <td>
+              <button class="btn btn-success btn-sm" id="btn_detail" data-toggle="modal" data-target="#modalDetailTagihan"><i class="fas fa-info"></i></button>
+            </td>
           </tr>`;
+          // tampilkan hasil pencarian
           $("#search_result").css('visibility', 'visible');
-          $("tbody").append(row);
+          $("#list_tagihan").append(row);
+          // hasil pencarian, detail tagihan mahasiswa
+          $("#detail_nim").val(data.nim);
+          $("#detail_nama_mhs").val(data.nama_mahasiswa);
+          $("#detail_nama_paket").val(data.detail_paket.nama_paket);
+          // hasil pencarian, item tagihan
+          var total_tagihan = 0;
+          for (let index = 0; index < data.item_paket.length; index++) {
+            total_tagihan += parseInt(data.item_paket[index].nominal_item);
+            var row1 = `<tr class="">
+            <td>${data.item_paket[index].id_item}</td>
+            <td>${data.item_paket[index].nama_item}</td>
+            <td>Rp ${data.item_paket[index].nominal_item}</td>
+            <td>${data.item_paket[index].keterangan_item}</td>
+            </tr>`;
+            $("#list_item").append(row1);
+          }
+          var row_total = `<tr class="font-weight-bold">
+          <td colspan="3">Total Tagihan</td>
+          <td>Rp ${total_tagihan}</td>
+          `;
+          $("#list_item").append(row_total);
         }
       },
-      error: function(jqXHR){
+      error: function(jqXHR) {
         console.log(jqXHR);
         alert("Data tidak ditemukan");
       }
     });
+  }
+
+  function detail() {
+
   }
 </script>
 <?= $this->endSection() ?>
