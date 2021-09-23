d42a552fd848a4ab6c42a61ec4d611b6f38cc3b1
AlvifSandana <asm240800@gmail.com>
2021-09-23 13:31:50 +0700

    add method index

--- app/Controllers/PembayaranController.php
+++ app/Controllers/PembayaranController.php
@@ -8,6 +8,8 @@ class PembayaranController extends BaseController
 {
     public function index()
     {
-        //
+        $request = \Config\Services::request();
+        $data['uri_segment'] = $request->uri->getSegment(1);
+        return view('pages/pembayaran/index', $data);
     }
 }
