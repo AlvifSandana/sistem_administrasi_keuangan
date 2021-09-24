b7e2f52a6e4eeb5dc9942dcee631e7839281c3d1
AlvifSandana <asm240800@gmail.com>
2021-09-24 11:04:16 +0700

    TODO: create pembayaran layout + controller

--- app/Controllers/PembayaranController.php
+++ app/Controllers/PembayaranController.php
@@ -3,6 +3,11 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ItemPaketModel;
+use App\Models\MahasiswaModel;
+use App\Models\PaketModel;
+use App\Models\PembayaranModel;
+use App\Models\TagihanModel;
 
 class PembayaranController extends BaseController
 {
@@ -11,5 +16,20 @@ class PembayaranController extends BaseController
         $request = \Config\Services::request();
         $data['uri_segment'] = $request->uri->getSegment(1);
         return view('pages/pembayaran/index', $data);
+    }
+
+    public function search_pembayaran()
+    {  
+        try {
+            // model
+            $m_tagihan = new TagihanModel();
+            $m_mahasiswa = new MahasiswaModel(); 
+            $m_paket = new PaketModel();
+            $m_itempaket = new ItemPaketModel();
+            $m_pembayaran = new PembayaranModel();
+            
+        } catch (\Throwable $th) {
+            return json_encode($th);
+        }
     }
 }
--- app/Views/pages/pembayaran/index.php
+++ app/Views/pages/pembayaran/index.php
@@ -21,6 +21,72 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('content-body') ?>
+<section class="content">
+  <div class="container-fluid">
+    <div class="row mb-1">
+      <div class="col">
+        <button class="btn btn-success float-right"><i class="fas fa-add"></i> Tambah Pembayaran</button>
+      </div>
+    </div>
+    <div class="row mb-2">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-9">
+                <input type="text" name="nim" id="nim" class="form-control" placeholder="Cari berdasarkan NIM">
+              </div>
+              <div class="col-3">
+                <button class="btn btn-primary btn-block" onclick=""><i class="fas fa-search"></i> Cari</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mb-2">
+      <div class="col">
+        <div class="card" id="search_result" style="visibility: hidden;">
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="h5">Hasil Pencarian</h5>
+                <table class="table table-hover">
+                  <thead class="text-center">
+                    <th>ID</th>
+                    <th>NIM</th>
+                    <th>NAMA MAHASISWA</th>
+                    <th>ACTION</th>
+                  </thead>
+                  <tbody class="text-center" id="list_pembayaran"></tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mb-2">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <table class="table table-hover">
+                  <thead class="text-center">
+                    <th>ID</th>
+                    <th>NAMA PEMBAYARAN</th>
+                  </thead>
+                  <tbody class="text-center"></tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?= $this->endSection() ?>
 
 <?= $this->section('custom-script') ?>
