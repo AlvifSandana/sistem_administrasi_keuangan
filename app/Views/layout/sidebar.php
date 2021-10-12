6f29b86b3d10863aebd1f8ff241d04d6148ba086
AlvifSandana <asm240800@gmail.com>
2021-10-12 22:45:05 +0700

    change sidebar item

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
+  <a href="#" class="brand-link">
     <img src="assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Keuangan</span>
   </a>
@@ -74,18 +74,10 @@
         </a>
       </li>
       <li class="nav-item">
-        <a href="<?php echo base_url() ?>/master-angkatan" class="nav-link">
-          <i class="nav-icon far fa-clock"></i>
+        <a href="<?php echo base_url() ?>/master-pendukung" class="nav-link">
+          <i class="nav-icon far fa-plus-square"></i>
           <p>
-            Data Tahun Angkatan
-          </p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="<?php echo base_url() ?>/master-progdi" class="nav-link">
-          <i class="nav-icon fas fa-graduation-cap"></i>
-          <p>
-            Data Program Studi
+            Data Pendukung
           </p>
         </a>
       </li>
