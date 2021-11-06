db7c1b3eeba406b2e0e471447eb45047d4c67998
AlvifSandana <asm240800@gmail.com>
2021-11-06 16:56:30 +0700

    update sidebar

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -2,8 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Keuangan</span>
+        <img src="assets/img/bill.png" class="brand-image elevation-2" alt="Brand" style="opacity: .8;"/>
+        <span class="brand-text font-weight-light">Sistem Keuangan</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,7 +11,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="assets/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="assets/img/admin.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="<?php echo base_url() ?>" class="d-block">Administrator
