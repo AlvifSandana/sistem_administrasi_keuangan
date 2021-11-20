010be99f687f3e79eeeb2e385bad908f2773770d
AlvifSandana <asm240800@gmail.com>
2021-11-20 18:03:19 +0700

    create lapran and backup restore controllers

---
+++ app/Controllers/Master/BackupRestoreController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Master;
+
+use App\Controllers\BaseController;
+
+class BackupRestoreController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/Master/LaporanController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Master;
+
+use App\Controllers\BaseController;
+
+class LaporanController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
