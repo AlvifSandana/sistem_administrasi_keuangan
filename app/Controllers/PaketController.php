2887daa7017c82210286005322a8bae130a0e16e
AlvifSandana <asm240800@gmail.com>
2021-12-05 22:50:59 +0700

    new method: update & delete paket

--- app/Controllers/PaketController.php
+++ app/Controllers/PaketController.php
@@ -103,33 +103,63 @@ class PaketController extends BaseController
                     return json_encode($result);
                 }
             } else {
+                $result['status'] = 'failed';
+                $result['message'] = 'Validasi gagal. Silahkan isi form dengan lengkap!';
+                $result['data'] = $validator->getErrors();
+                return json_encode($result);
             }
         } catch (\Throwable $th) {
             $result['status'] = 'error';
             $result['message'] = $th->getMessage();
-            $result['data'] = [];
+            $result['data'] = $th->getTrace();
             return json_encode($result);
         }
     }
 
     public function update_paket($id)
     {
-        // TODO - create method for update a paket by id
         try {
-            // create model instance
-            $m_paket = new PaketModel();
-            // get data from request
-            $data = [
-                'nama_paket' => $this->request->getPost('nama_paket'),
-                'keterangan_paket' => $this->request->getPost('keterangan_paket'),
-                'semester_id' => $this->request->getPost('semester_id')
-            ];
-            // update data to dataabase
-            $m_paket->update($id, $data);
-            // return
-
+            // create validator
+            $validator = \Config\Services::validation();
+            // set validator rules
+            $validator->setRules([
+                'nama_paket' => 'required',
+                'semester_id' => 'required',
+            ]);
+            // validation check
+            $isDataValid = $validator->withRequest($this->request)->run();
+            if ($isDataValid) {
+                // create model instance
+                $m_paket = new PaketModel();
+                // get data from request
+                $data = [
+                    'nama_paket' => $this->request->getPost('nama_paket'),
+                    'keterangan_paket' => $this->request->getPost('keterangan_paket'),
+                    'semester_id' => $this->request->getPost('semester_id')
+                ];
+                // update data to dataabase
+                if ($m_paket->update($id, $data)) {
+                    $result['status'] = 'success';
+                    $result['message'] = 'Berhasil memperbarui paket!';
+                    $result['data'] = $data;
+                    return json_encode($result);
+                } else {
+                    $result['status'] = 'failed';
+                    $result['message'] = 'Gagal memperbarui paket.';
+                    $result['data'] = [];
+                    return json_encode($result);
+                }
+            } else {
+                $result['status'] = 'failed';
+                $result['message'] = 'Validasi gagal. Silahkan isi form dengan lengkap!';
+                $result['data'] = $validator->getErrors();
+                return json_encode($result);
+            }
         } catch (\Throwable $th) {
-            //throw $th;
+            $result['status'] = 'error';
+            $result['message'] = $th->getMessage();
+            $result['data'] = $th->getTrace();
+            return json_encode($result);
         }
     }
 
@@ -141,8 +171,21 @@ class PaketController extends BaseController
             $m_paket = new PaketModel();
             // delete paket by given id
             if ($m_paket->delete($id)) {
+                $result['status'] = 'success';
+                $result['message'] = 'Berhasil menghapus paket!.';
+                $result['data'] = [];
+                return json_encode($result);
+            } else {
+                $result['status'] = 'failed';
+                $result['message'] = 'Gagal menghapus paket.';
+                $result['data'] = [];
+                return json_encode($result);
             }
         } catch (\Throwable $th) {
+            $result['status'] = 'error';
+            $result['message'] = $th->getMessage();
+            $result['data'] = $th->getTrace();
+            return json_encode($result);
         }
     }
 }
