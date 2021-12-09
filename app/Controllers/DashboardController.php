f9ad0fbd38a1c01f114468d6b829ad3bb8296264
AlvifSandana <asm240800@gmail.com>
2021-12-09 22:25:44 +0700

    show total_tagihan & total_pembayaran

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -8,8 +8,24 @@ class DashboardController extends BaseController
 {
     public function index()
     {
+        // create request instance
         $request = \Config\Services::request();
+        // get uri segment
         $data['uri_segment'] = $request->uri->getSegment(1);
+        // get nominal tagihan & pembayaran keseluruhan mahasiswa
+        $db = \Config\Database::connect();
+        $builder_tagihan = $db->table('item_paket');
+        $builder_pembayaran= $db->table('pembayaran');
+        $query1 = $builder_tagihan
+            ->select('SUM(item_paket.nominal_item) as total_tagihan')
+            ->join('tagihan', 'item_paket.paket_id = tagihan.paket_id')
+            ->get();
+        $query2 = $builder_pembayaran
+            ->select('SUM(nominal_pembayaran) as total_pembayaran')
+            ->get();
+        $data['total_tagihan'] = $query1->getResultArray();
+        $data['total_pembayaran'] = $query2->getResultArray();
+        // return view with data
         return view('pages/dashboard', $data);
     }
 }
--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -23,7 +23,56 @@
 <?= $this->section('content-body') ?>
 <section class="content">
     <div class="container-fluid">
-
+        <div class="row mb-2">
+            <div class="col-6">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>Rp <?php echo number_format($total_tagihan[0]['total_tagihan'], 0); ?></h3>
+                        <p>Tagihan Keseluruhan Mahasiswa</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>Rp <?php echo number_format($total_pembayaran[0]['total_pembayaran'], 0); ?></h3>
+                        <p>Pembayaran Keseluruhan Mahasiswa</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-3">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>500</h3>
+                        <p>Jumlah Mahasiswa</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-users"></i>
+                    </div>
+                    <a href="<?php echo base_url(); ?>/master-mahasiswa" class="small-box-footer">More Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>4</h3>
+                        <p>Program Studi</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-book"></i>
+                    </div>
+                    <a href="<?php echo base_url(); ?>/master-pendukung" class="small-box-footer">More Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 <?= $this->endSection() ?>
